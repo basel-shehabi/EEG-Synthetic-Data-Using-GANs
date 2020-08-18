b1c93f7d7d7e692c2d5759c861afb2c30b2987f5
basel-shehabi <baselshehabi1@gmail.com>
2020-08-18 04:44:57 +0100

    updated dummy script ever so slightly....

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
+# Dummy script used just for testing .npz files and loading. 
 import numpy as np
 
-x = np.load('D:\Ciaran Python Data\Transfer-SARzWXT6VsetANd8\motorImagery\S1Processeds.npz')
-print(len(x))
-print(len(x['x']))
+x = np.load('D:\Ciaran Python Data\Transfer-SARzWXT6VsetANd8\motorImagery\Subject1Test1.npz')
 new_x = x['x']
 print(new_x.shape)
