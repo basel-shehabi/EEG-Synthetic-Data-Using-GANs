e2037666b558421504af64780275784d27b169ea
Basel Shehabi <44100599+basel-shehabi@users.noreply.github.com>
2020-08-30 21:06:07 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-# Dummy script used just for testing .npz files and loading.
-import numpy as np
-
-x = np.load('D:\Ciaran Python Data\Transfer-SARzWXT6VsetANd8\motorImagery\Subject1Test1.npz')
-new_x = x['x']
-print(new_x.shape)
